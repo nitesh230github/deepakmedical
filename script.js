c459280905601b54b0be8a722986d11717a0a47e
Nitesh Prajapati <38858865+nitesh230github@users.noreply.github.com>
2026-06-19 14:16:59 +0530

    Update script.js
    Part A
    Product Loading
    Search
    Add to Cart

--- script.js
+++ script.js
@@ -1,35 +1,13 @@
 let cart = [];
 
-fetch('products.json')
+fetch("products.json")
 .then(response => response.json())
 .then(products => {
 
-    const productsDiv = document.getElementById('products');
-
-    function displayProducts(items) {
-
-        productsDiv.innerHTML = '';
-
-        items.forEach(product => {
-
-            productsDiv.innerHTML += `
-            <div class="card">
-                <img src="${product.image}">
-                <h3>${product.name}</h3>
-                <p class="price">₹${product.price}</p>
-
-                <button onclick="addToCart('${product.name}',${product.price})">
-                    Add to Cart
-                </button>
-
-            </div>
-            `;
-        });
-    }
-
     displayProducts(products);
 
-    document.getElementById('search').addEventListener('keyup', function(){
+    document.getElementById("search")
+    .addEventListener("keyup", function(){
 
         let value = this.value.toLowerCase();
 
@@ -43,138 +21,57 @@ fetch('products.json')
 
 });
 
+function displayProducts(items){
 
-function addToCart(name, price){
+    let html = "";
+
+    items.forEach(product => {
+
+        html += `
+        <div class="card">
+
+            <img src="${product.image}" alt="${product.name}">
+
+            <h3>${product.name}</h3>
+
+            <p class="price">₹${product.price}</p>
+
+            <button
+            onclick="addToCart('${product.name}',${product.price})">
+
+            Add to Cart
+
+            </button>
+
+        </div>
+        `;
+
+    });
+
+    document.getElementById("products").innerHTML = html;
+
+}
+
+function addToCart(name,price){
 
     let item = cart.find(x => x.name === name);
 
     if(item){
+
         item.qty++;
-    }
-    else{
+
+    }else{
+
         cart.push({
+
             name:name,
             price:price,
             qty:1
+
         });
+
     }
 
     showCart();
-}
-
-
-function showCart(){
-
-    let html = `
-    <hr>
-    <h2>🛒 Cart</h2>
-    `;
-    let total = 0;
-    cart.forEach((item,index)=>{
-        total += item.price * item.qty;
-        html += `
-        <p>
-        ${item.name}
-
-        <button onclick="decreaseQty(${index})">-</button>
-
-        ${item.qty}
-
-        <button onclick="increaseQty(${index})">+</button>
-
-        </p>
-        `;
-    });
-    html += `
-    <h3 class="total">
-    Total ₹${total}
-    </h3>
-     `;
-    html += `
-
-    <br><br>
-
-    <input id="customerName" placeholder="Customer Name">
-
-    <br><br>
-
-    <input id="customerMobile" placeholder="Mobile Number">
-
-    <br><br>
-
-    <button onclick="sendOrder()">
-    Order on WhatsApp
-    </button>
-
-    `;
-
-    let oldCart = document.getElementById("cartArea");
-
-    if(oldCart){
-    oldCart.remove();
-}
-
-    document.getElementById("products").insertAdjacentHTML(
-    "afterend",
-    `<div id="cartArea">${html}</div>`
-);
-}
-
-
-function increaseQty(index){
-
-    cart[index].qty++;
-
-    document.getElementById("cartArea").remove();
-
-    showCart();
-
-}
-
-
-function decreaseQty(index){
-
-    if(cart[index].qty>1){
-        cart[index].qty--;
-    }
-
-    document.getElementById("cartArea").remove();
-
-    showCart();
-
-}
-
-
-function sendOrder(){
-
-    let name = document.getElementById("customerName").value;
-
-    let mobile = document.getElementById("customerMobile").value;
-   
-    if(name==="" || mobile===""){
-        alert("Please enter Name and Mobile Number");
-        return;
-    }
-
-    let msg =
-`Hello Deepak Medical Agency
-
-Customer Name: ${name}
-
-Mobile Number: ${mobile}
-
-Order Details: 
-
-`;
-
-    cart.forEach(item=>{
-
-        msg += `${item.name} × ${item.qty}\n`;
-
-    });
-
-    window.open(
-"https://wa.me/917804008789?text="+encodeURIComponent(msg)
-);
 
 }
