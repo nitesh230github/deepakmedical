cf9c58123e3349df00027b64918cfff446322c77
Nitesh Prajapati <38858865+nitesh230github@users.noreply.github.com>
2026-06-20 11:23:08 +0530

    Update script.js
    random shuffle of items on refresh

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 let cart = [];
 
+function shuffleArray(array){
+
+    let arr = [...array];
+
+    for(let i = arr.length - 1; i > 0; i--){
+
+        let j = Math.floor(Math.random() * (i + 1));
+
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+
+    return arr;
+
+}
+
 fetch("products.json")
 .then(response => response.json())
 .then(products => {
